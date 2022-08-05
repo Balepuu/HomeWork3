7b6972510000531f740da515901ee6d52c42942d
ааа <patigorsk1995@mail.ru>
2022-08-05 12:42:14 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 19;
-        if (age >= 18 && age < 21) {
-            System.out.println("Ты достаточно взрослый для того, чтобы ходить,но не достаточно, чтобы пить алкоголь");
-        }
-        if (age < 7 || age > 18) {
-            System.out.println("Я думаю ты не ходишь в школу");
+
+        boolean clientOs = 1;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else  if (clientOs == 0) {
+            System.out.println("Установите версию приложения для Андроид по ссылке");
+        } else {
+            System.out.println("ОС не найдена");
         }
     }
 }
